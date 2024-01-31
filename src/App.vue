475db30eb80716b518111fcd40999f7990ba7ce8
ntnyq <ntnyq13@gmail.com>
2024-01-31 13:24:53 +0800

    chore: init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import VButton from './VButton.vue'
+
+const handleClick = () => {
+  console.log('clicked')
+}
+</script>
+
+<template>
+  <VButton @update="handleClick">Click me</VButton>
+</template>
---
+++ src/VButton.vue
@@ -0,0 +1,19 @@
+<script>
+import { defineComponent, h } from 'vue'
+
+export default defineComponent({
+  emits: ['update'],
+
+  setup(_, ctx) {
+    return () =>
+      h(
+        'button',
+        {
+          type: 'button',
+          onClick: () => ctx.emit('update'),
+        },
+        'Click me!',
+      )
+  },
+})
+</script>
